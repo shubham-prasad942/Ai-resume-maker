c2581541498da70a4ef2f02f56ba63a0b75bdbd1
Shubham Prasad <shubhamprasad942@gmail.com>
2026-03-17 20:18:54 +0530

    fix image path for production

--- src/Componets/Hero.jsx
+++ src/Componets/Hero.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 // Hero component
 const Hero = () => {
   return (
-    <div className="py-5 flex flex-col md:flex-row gap-2 sm:gap-6">
+    <div className="py-5 flex flex-col md:flex-row gap-7 sm:gap-6">
       <div className="md:w-1/2 flex-1 space-y-3.5">
         <div className="text-sm sm:text-base text-white bg-primary rounded-xl w-fit px-3 py-0.5 font-medium ">
           <i className="ri-bard-fill"></i>  AI-Powered Resume Builder
