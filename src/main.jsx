521f133ac703d4821a9d402669e5cc3815b4fe24
Shubham Prasad <shubhamprasad942@gmail.com>
2026-04-07 17:41:29 +0530

    fix import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from "./router/Router.jsx"
+import router from "./router/router.jsx"
 import ResumeProvider from "./components/context/ResumeContext.jsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
